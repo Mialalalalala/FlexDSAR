a6d9f480ddbe7965d616fbb1ef291d8b9bf783c7
Yuhuan Zhao <yuhuanzh@usc.edu>
2025-02-24 22:37:04 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ if st.button("Run Comparison"):
         ax1.set_xlabel("Cases")
 
         ax1.set_ylabel("SM RMSE (m³/m³)")
-        ax1.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+        ax1.legend(bbox_to_anchor=(1.3, 1.1), loc='upper right')
         
         # Add VWC RMSE plot
         ax2 = ax1.twinx()
@@ -139,7 +139,7 @@ if st.button("Run Comparison"):
         vwc_max_value = max(case["rmse"][5] for case in st.session_state.comparison_cases)
         ax2.set_ylim(0, vwc_max_value+0.05)
         ax2.set_ylabel("VWC RMSE (kg/m²)")
-        ax2.legend(loc='upper right', bbox_to_anchor=(1.27, 0.9))
+        ax2.legend(bbox_to_anchor=(1.27, 0.9), loc='upper right')
         plt.grid()
         st.pyplot(fig)
         
